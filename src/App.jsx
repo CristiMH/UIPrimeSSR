3b6eb3e4d9afa7b13a868e9e0e8dc5faa3fe084e
CristiMH <mihaila.cristian13@gmail.com>
2025-04-24 16:16:58 +0300

    design

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ import Pricing from "./components/Pricing";
 import Contact from "./components/Contact";
 import languageContext from "./contexts/languageContext";
 import ScrollButton from "./components/ScrollButton";
+import { motion, useScroll } from "motion/react"
 
 function App() {
   const [language, setLanguage] = useState('ro');
+  const { scrollYProgress } = useScroll()
 
   useEffect(() => {
     const storedLang = localStorage.getItem('language');
@@ -27,6 +29,20 @@ function App() {
   return (
     <languageContext.Provider value={{ language, setLanguage }}>
       <>
+        <motion.div
+          id="scroll-indicator"
+          style={{
+            scaleX: scrollYProgress,
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 2,
+            originX: 0,
+            backgroundColor: "#28b5af"
+          }}
+          className="z-[999]"
+        />
         <LanguageSelector />
         <Top onScrollTo={scrollToSection} />
         <WorkSwiper />
